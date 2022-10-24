60cd53cb8a07ff3f382d9c37deffca2c0b740b62
Maxim Semeniuk <semeniuk.maksim@gmail.com>
2022-10-24 23:13:48 +0300

    Update task-05.js

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -2,8 +2,8 @@ const textRef = document.querySelector('#name-input');
 const spanRef = document.querySelector('#name-output');
 let spanText = ' ';
 
-const addText = textRef.addEventListener('input', () => {
-	if (textRef) {
+const addText = textRef.addEventListener('input', value => {
+	if (textRef === value) {
 		spanRef.textContent = textRef;
 	}
 	spanRef.textContent = 'Anonymous';
